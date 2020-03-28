0aea22559870dc11d7313bcf48b116c8d0c9f5b8
ANAMPEDIA <namdevel@users.noreply.github.com>
2020-03-28 23:29:45 +0700

    Update example.py

--- example.py
+++ example.py
@@ -1,9 +1,9 @@
 from src.corona import Covid19
 
-c = Covid19()
-print(c.getdata()) # data global
-print(c.getdata()['cases'])  # data global [cases, deats, recovered]
+ndvl = Covid19()
+print(ndvl.getdata()) # data global
+print(ndvl.getdata()['cases'])  # data global [cases, deats, recovered]
 
-print(c.getdata("indonesia")) # data by country
-print(c.getdata("indonesia")['recovered']) # data by country [cases, deats, recovered]
+print(ndvl.getdata("indonesia")) # data by country
+print(ndvl.getdata("indonesia")['recovered']) # data by country [cases, deats, recovered]
 
